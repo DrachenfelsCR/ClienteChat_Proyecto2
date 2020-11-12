efcd6e0457e93e38eb68660bcdc02d33d921b684
David <David@DESKTOP-28KAJJM>
2020-11-11 22:24:50 -0600

    Completo

--- src/chatClient/Application.java
+++ src/chatClient/Application.java
@@ -1,7 +1,7 @@
 package chatClient;
 
 public class Application {
-
+//David Quesada Romero - Alberto Esquivel Gomez - Andres Salas Miranda
     public static void main(String[] args) {
        Model model = new Model();
         View view= new View();
